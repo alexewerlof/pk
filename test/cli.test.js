5d2a3d1da98d62566077a776992ce464e8639413
UserPixel <hanifbbz@gmail.com>
2018-01-30 21:48:09 +0100

    improve cli test runner

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,36 +1,43 @@
 const assert = require('assert');
 const fs = require('fs');
-const path = require('path');
+const { join } = require('path');
 const { promisify } = require('util');
 const childProcess = require('child_process');
 const { expect } = require('chai');
 
 const readFile = promisify(fs.readFile);
 
-const tests = [
-    './test/io/get1.txt'
-];
-
 function runCli(args) {
     return new Promise(resolve => {
-        childProcess.execFile(path.join(__dirname, '../bin/cli.js'), args, { cwd: path.join(__dirname, 'io') }, (error, stdout, stderr) => {
+        childProcess.execFile(join(__dirname, '../bin/cli.js'), args, { cwd: join(__dirname, 'io') }, (error, stdout, stderr) => {
             assert.ifError(error);
             resolve(stdout);
         });
     });
 }
 
-describe('cli', () => {
-    tests.forEach((o) => {
-        
-        it(o, async () => {
-            const testCase = (await readFile(o)).toString();
-            const testCaseLines = testCase.split('\n');
-            assert.ok(testCaseLines.length > 3, `test case should contain description, params and expected output ${testCase}`);
-            const [ desc, args, ...expectedOutput ] = testCaseLines;
-        
-            expect(await runCli(args.split(' '))).to.equal(expectedOutput.join('\n'));
-        });
+async function readTestCase(fileName) {
+    const testCase = (await readFile(join(__dirname, 'io', fileName))).toString();
+    const testCaseLines = testCase.split('\n');
+    assert.ok(testCaseLines.length > 3, `test case should contain description, params and expected output ${testCase}`);
+    const [ desc, args, ...output ] = testCaseLines;
+    return {
+        desc,
+        args: args.split(' '), //Bogus for when we use quotations
+        output: output.join('\n')
+    };
+}
 
-    });
+describe('cli', () => {
+    fs.readdirSync(join(__dirname, 'io'))
+        .filter(fileName => /\.txt$/i.test(fileName))
+        .forEach((fileName) => {
+        
+            it(fileName, async () => {
+                const { args, output } = await readTestCase(fileName);
+            
+                expect(await runCli(args)).to.equal(output);
+            });
+
+        });
 });
